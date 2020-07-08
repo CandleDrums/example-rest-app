e616ff9743dcc127d2a262dded67efa89623625b
liming <liming@limingdembp>
2020-07-08 18:34:24 +0800

    修改

---
+++ example-web/src/main/java/com/cds/app/example/web/config/Swagger2Config.java
@@ -0,0 +1,48 @@
+/**
+ * @Project example-web
+ * @Package com.cds.app.example.web.config
+ * @Class Swagger2Config.java
+ * @Date Jul 8, 2020 6:32:49 PM
+ * @Copyright (c) 2020 CandleDrumS.com All Right Reserved.
+ */
+package com.cds.app.example.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Description swagger配置
+ * @Notes 未填写备注
+ * @author [author]
+ * @Date [date]
+ */
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+            .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Example Rest Api Doc")
+            .contact(new Contact("ming.li", "http://www.candledrums.com", "ming.li@mail.candledrums.com"))
+            .description("Example Rest Api文档").version("1.0").build();
+    }
+
+}
